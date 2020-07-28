617f169295986f4dd7d9997c8bcf202725079e72
Jeff <zjyeung579@gmail.com>
2020-07-27 18:10:31 -0700

    Changed logo. Added styling to h1, h2 of the site

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -31,17 +31,19 @@ const LayoutContainer = styled.div`
     }
 `;
 
+const myStaticQuery = graphql`
+    query SiteTitleQuery {
+        site {
+            siteMetadata {
+                title
+            }
+        }
+    }
+`;
+
 const Layout = ({ children }) => (
     <StaticQuery
-        query={graphql`
-            query SiteTitleQuery {
-                site {
-                    siteMetadata {
-                        title
-                    }
-                }
-            }
-        `}
+        query={`${myStaticQuery}`}
         render={data => (
             <LayoutContainer className="div">
                 <Global styles={[globalStyles, typeStyles]} />
