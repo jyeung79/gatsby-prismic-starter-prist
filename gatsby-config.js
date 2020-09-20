d7917b465a6eaa3769a20623a6c7ddcd9624f721
Jeff Yeung <zjyeung579@gmail.com>
2020-09-19 17:31:35 -0700

    Replaced Doge.png with webp file for icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,7 +48,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/Doge.png`, // This path is relative to the root of the site.
+        icon: `src/images/Doge.webp`, // This path is relative to the root of the site.
       },
     },
     // https://www.gatsbyjs.org/packages/gatsby-plugin-google-analytics/
